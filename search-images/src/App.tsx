7b370c43edebb544acc60ba790029baec7545ac5
sagar2901 <sagar2901@hotmail.com>
2022-09-20 23:40:03 -0400

    Getting response from pixel app

--- search-images/src/App.tsx
+++ search-images/src/App.tsx
@@ -3,15 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import {getImages} from './service';
 
+
+
 function App() {
 
-  const [images, setImages] = useState(null);
+  const [photos, setPhotos] = useState<any>(null);
   const [query, setQuery] = useState('');
 
   const search = () => {
     getImages(query).then(result => {
       console.log(result);
-      setImages(result);
+      setPhotos(result.photos);
     });
   }
 
@@ -26,6 +28,11 @@ function App() {
           value={query}
           onChange={handleChange}/>
           <button onClick={search}>Search</button>
+          <div>
+            {photos && photos.map((photo: any)=> {
+              return <div key={photo.id}><img src={photo.src.small} alt={photo.alt}/></div>
+            })}
+          </div>
     </div>
   );
 }
